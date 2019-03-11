09eda709e21c0190ba3cbe799bec3acfbbcea5bf
FancyPantsss <schrauwen.nina@gmail.com>
2019-03-11 12:38:26 +0100

    no message

--- smartpool/app/src/main/java/com/example/smartpool/MainActivity.java
+++ smartpool/app/src/main/java/com/example/smartpool/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         String a = new String("bfkj!");
-        int ietsofzo = 0;
+        int ietsofzo = 3;
     }
 }
