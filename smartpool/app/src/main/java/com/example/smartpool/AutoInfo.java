94c32d15b47f2cb3ecd94a178e7f8d11742662e2
FancyPantsss <schrauwen.nina@gmail.com>
2019-03-18 10:14:13 +0100

    hoi

--- smartpool/app/src/main/java/com/example/smartpool/AutoInfo.java
+++ smartpool/app/src/main/java/com/example/smartpool/AutoInfo.java
@@ -1,4 +1,38 @@
 package com.example.smartpool;
 
 public class AutoInfo {
+
+    private String kenteken;
+    private String merk;
+    private String kleur;
+
+    public AutoInfo(String kenteken, String merk, String kleur) {
+        this.kenteken = kenteken;
+        this.merk = merk;
+        this.kleur = kleur;
+    }
+
+    public String getKenteken() {
+        return kenteken;
+    }
+
+    public void setKenteken(String kenteken) {
+        this.kenteken = kenteken;
+    }
+
+    public String getMerk() {
+        return merk;
+    }
+
+    public void setMerk(String merk) {
+        this.merk = merk;
+    }
+
+    public String getKleur() {
+        return kleur;
+    }
+
+    public void setKleur(String kleur) {
+        this.kleur = kleur;
+    }
 }
--- smartpool/app/src/main/java/com/example/smartpool/MainActivity.java
+++ smartpool/app/src/main/java/com/example/smartpool/MainActivity.java
@@ -1,16 +1,30 @@
 package com.example.smartpool;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String a = new String("bfkj!");
-        int ietsofzo = 3;
+
+        final FloatingActionButton button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent go = new Intent(MainActivity.this, AddActivity.class);
+                startActivity(go);
+
+            }
+        });
+
+
     }
 }
