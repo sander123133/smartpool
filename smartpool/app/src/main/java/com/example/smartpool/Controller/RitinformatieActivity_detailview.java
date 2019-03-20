3603674f88247ed51db68f88ffce307e1099c470
MSI\Sander <sjaakdesteen@gmail.com>
2019-03-20 10:25:44 +0100

    fixed bug with scrolling almost finished with the layout

---
+++ smartpool/app/src/main/java/com/example/smartpool/Controller/RitinformatieActivity_detailview.java
@@ -0,0 +1,16 @@
+package com.example.smartpool.Controller;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.smartpool.R;
+
+public class RitinformatieActivity_detailview  extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detailview_ritinformatie);
+    }
+}
